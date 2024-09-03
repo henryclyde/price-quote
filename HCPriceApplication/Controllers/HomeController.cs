a883133c0e1bca9c00e936ed0a5235c51ad05ee4
henryclyde <hen.clyde@gmail.com>
2024-09-03 16:18:51 -0600

    Add project files.

---
+++ HCPriceApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HCPriceApplication.Models;
+
+namespace HCPriceApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet] // http get request
+        public IActionResult Index() // Index method view returned
+        {
+            ViewBag.Discount = 0; // discount variable declared
+            ViewBag.Total = 0; // total variable declared
+            return View(); // view returned
+        }
+
+        [HttpPost] // http post request
+        public IActionResult Index(Quotation model) //index view with calculations calculated
+        {
+            if (ModelState.IsValid) // if the model state is valid..
+            {
+                ViewBag.Discount = model.CalculateDiscount(); // set discount variable in view model to the calculated discount
+                ViewBag.Total = model.CalculateTotal(); // set total variable in view model to the calculated total
+            }
+            else // if model state isnt valid..
+            {
+                ViewBag.Discount = 0; // keep variables at 0
+                ViewBag.Total = 0; // keep variables at 0
+            }
+            return View(model); //return completed model
+
+        }
+    }
+}
---
+++ HCPriceApplication/Models/Quotation.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HCPriceApplication.Models
+{
+    public class Quotation
+    {
+        [Required(ErrorMessage = "Please enter a Subtotal.")]
+        [Range(0.01, 99999999, ErrorMessage = "Subtotal must be greater than 0")]
+        public decimal? Subtotal { get; set; } // subtotal declaration, must be over 0
+
+        [Required(ErrorMessage = "Please enter a Discount.")]
+        [Range(0, 100, ErrorMessage =
+               "Discount percent must be between 0 and 100")]
+        public decimal? Discount { get; set; } // discount declaration, must be between 0 and 100
+
+        public decimal? CalculateDiscount() // method to calculate discount
+        {
+            decimal? discountAmount = Subtotal * (Discount / 100); // calculation
+            return discountAmount; // return value
+        }
+
+        public decimal? CalculateTotal() // method to calculate total
+        {
+            decimal? total = Subtotal - ((Discount / 100) * Subtotal); // calculation
+            return total; // return value
+        }
+    }
+}
